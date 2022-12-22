02da51d488b732ad3c7e9a5bc5a5793975649df0
KafetzisThomas <tomkafetzis06@gmail.com>
2022-12-22 20:03:40 +0200

    Fetch download & upload speed

--- my_ip.py
+++ my_ip.py
@@ -4,9 +4,12 @@
 # Project Title: my-ip (https://github.com/KafetzisThomas/my-ip)
 # Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
 
-import sys, socket, time, colorama
+# Import built in modules
+import sys, socket, time, speedtest
 from requests import get
 from datetime import datetime
+# Import other modules
+import colorama
 from colorama import Fore as F
 colorama.init(autoreset=True)
 
@@ -14,6 +17,12 @@ try:
     # Get current time in a 24Hour format
     currentDateAndTime = datetime.now()
     currentTime = currentDateAndTime.strftime("%H:%M:%S")
+    
+    st = speedtest.Speedtest()
+    
+    # Fetch download & upload speed
+    download = st.download()
+    upload = st.upload()
     
     input(f"\nPress {F.GREEN}Enter{F.RESET} to Continue/{F.RED}Ctrl+C{F.RESET} to Stop")
     while True:
@@ -27,6 +36,8 @@ try:
         print(f"Your Computer Name is: {F.GREEN}{hostname}")
         print(f"Your Computer IP Address is: {F.GREEN}{LocalIp}")
         print(f"Your Public IP Address is: {F.RED}{PublicIp}")
+        print(f"Download Speed: {F.GREEN}{'%.2f' % (download / 1_000_000)} Mbps")
+        print(f"Upload Speed: {F.GREEN}{'%.2f' % (upload / 1_000_000)} Mbps")
 
 except KeyboardInterrupt:
     print(f"{F.RED}Operation canceled.")
