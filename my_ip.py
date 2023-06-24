e170003c265f276a51f54c0cc9f6e374792d5e17
KafetzisThomas <tomkafetzis06@gmail.com>
2023-06-24 10:54:39 +0300

    Remove speedtest because of load problems

--- my_ip.py
+++ my_ip.py
@@ -5,52 +5,34 @@
 # Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
 
 # Import built in modules
-import sys, socket, time, speedtest
+import sys, socket, time
 from requests import get
 from datetime import datetime
+# Get current time in a 24Hour format
+CURRENT_TIME = datetime.now().strftime("%H:%M:%S")
+
 # Import other modules
 import colorama
 from colorama import Fore as F
 colorama.init(autoreset=True)
 
 print("\nIt's searching data according your network. This may take a few seconds...")
+input(f"\nPress {F.GREEN}Enter{F.RESET} to Continue/{F.RED}Ctrl+C{F.RESET} to Stop")
 
 try:
-    # Get current time in a 24Hour format
-    currentDateAndTime = datetime.now()
-    currentTime = currentDateAndTime.strftime("%H:%M:%S")
-    
-    st = speedtest.Speedtest()
-    
-    # Fetch download & upload speed
-    download = st.download()
-    upload = st.upload()
+  while True:
+    time.sleep(1)
+    print("-" * 42)
 
-    # Fetch ping
-    st.get_servers()
-    ping = st.results.ping
-    
-    input(f"\nPress {F.GREEN}Enter{F.RESET} to Continue/{F.RED}Ctrl+C{F.RESET} to Stop")
-    while True:
-        time.sleep(1)
-        print("-" * 42)
-        
-        # Fetch computer name
-        hostname = socket.gethostname()
-        # Fetch computer ip address
-        LocalIp = socket.gethostbyname(hostname)
-        # Fetch public ip address
-        PublicIp = get('https://api.ipify.org').text
-  
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OUTPUT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-        print(f"Current Time:        {F.GREEN}{currentTime}")
-        print(f"Computer Name:       {F.GREEN}{hostname}")
-        print(f"Computer IP Address: {F.GREEN}{LocalIp}")
-        print(f"Public IP Address:   {F.RED}{PublicIp}")
-        print(f"Download Speed:      {F.GREEN}{'%.2f' % (download / 1_000_000)} Mbps")
-        print(f"Upload Speed:        {F.GREEN}{'%.2f' % (upload / 1_000_000)} Mbps")
-        print(f"Ping:                {F.GREEN}{ping}")
+    HOSTNAME = socket.gethostname()  # Fetch computer name
+    LOCAL_IP = socket.gethostbyname(HOSTNAME)  # Fetch computer ip address
+    PUBLIC_IP = get('https://api.ipify.org').text  # Fetch public ip address
 
+    #~~~~~~~~~~~~~~~~~~~~~ OUTPUT ~~~~~~~~~~~~~~~~~~~~~~~~#
+    print(f"Current Time:        {F.GREEN}{CURRENT_TIME}")
+    print(f"Computer Name:       {F.GREEN}{HOSTNAME}")
+    print(f"Computer IP Address: {F.GREEN}{LOCAL_IP}")
+    print(f"Public IP Address:   {F.RED}{PUBLIC_IP}")
 except KeyboardInterrupt:
-    print(f"{F.RED}Operation canceled.")
-    sys.exit()
+  print(f"{F.RED}Operation canceled.")
+  sys.exit()
